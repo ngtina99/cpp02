1ddf36da8622e692f73a2fd8322a2fa61418f6a8
Tina <nguyen.valentina.99@gmail.com>
2024-10-06 21:31:29 +0200

    ok

--- ex00/Fixed.hpp
+++ ex00/Fixed.hpp
@@ -1,1 +1,38 @@
-Ö
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Fixed.hpp                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: thuy-ngu <thuy-ngu@student.42lisboa.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/10/06 20:45:49 by thuy-ngu          #+#    #+#             */
+/*   Updated: 2024/10/06 20:45:49 by thuy-ngu         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FIXED_HPP
+# define FIXED_HPP
+
+# include <iostream>
+
+class	Fixed
+{
+
+	public:
+
+		Fixed();
+		~Fixed();
+		Fixed( Fixed const &src );
+		Fixed	&operator=( Fixed const &rhs );
+
+		int		getRawBits() const;
+		void	setRawBits( int const raw );
+
+	private:
+
+	int					_value;
+	static const int	_bits = 8;
+
+};
+
+#endif
---
+++ ex00/main.cpp
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: thuy-ngu <thuy-ngu@student.42lisboa.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/10/06 20:44:36 by thuy-ngu          #+#    #+#             */
+/*   Updated: 2024/10/06 20:44:36 by thuy-ngu         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Fixed.hpp"
+
+int main( void ) {
+Fixed a;
+Fixed b( a );
+Fixed c;
+c = b;
+std::cout << a.getRawBits() << std::endl;
+std::cout << b.getRawBits() << std::endl;
+std::cout << c.getRawBits() << std::endl;
+return
